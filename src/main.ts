bf7ee449f26f5b5f3afcdae992c4811c8adcf0e0
Lauren McCall <lmmccolloch@gmail.com>
2022-03-16 16:19:54 -0400

    player 1 and 2 throws queryselectors up

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,15 @@
 import './style.css'
 
 console.clear()
+
+// Player one selects throw
+const playerOneRock = document.querySelector('.player1 i.rock')
+const playerOnePaper = document.querySelector('.player1 i.paper')
+const playerOneScissors = document.querySelector('.player1 i.scissors')
+
+// Player two selects throw
+const playerTwoRock = document.querySelector('.player2 i.rock')
+const playerTwoPaper = document.querySelector('.player2 i.paper')
+const playerTwoScissors = document.querySelector('.player2 i.scissors')
+
+// Display winner
