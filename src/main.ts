1cac01865243a2ce5f4bdcdcd9381271b384b9c3
Lauren McCall <lmmccolloch@gmail.com>
2022-03-16 16:48:30 -0400

    functions for player 1 and 2 clicking on rock, paper, or scissors

--- src/main.ts
+++ src/main.ts
@@ -12,4 +12,45 @@ const playerTwoRock = document.querySelector('.player2 i.rock')
 const playerTwoPaper = document.querySelector('.player2 i.paper')
 const playerTwoScissors = document.querySelector('.player2 i.scissors')
 
+let playerOneChoice: string
+let playerTwoChoice: string
+let result: string
+
+// Functions for clicking on rock, paper, or scissors
+function clickOnRockPlayerOne() {
+  playerOneChoice = 'rock'
+  console.log(playerOneChoice)
+}
+playerOneRock?.addEventListener('click', clickOnRockPlayerOne)
+
+function clickOnPaperPlayerOne() {
+  playerOneChoice = 'paper'
+  console.log(playerOneChoice)
+}
+playerOnePaper?.addEventListener('click', clickOnPaperPlayerOne)
+
+function clickOnScissorsPlayerOne() {
+  playerOneChoice = 'scissors'
+  console.log(playerOneChoice)
+}
+playerOneScissors?.addEventListener('click', clickOnScissorsPlayerOne)
+
+function clickOnRockPlayerTwo() {
+  playerTwoChoice = 'rock'
+  console.log(playerTwoChoice)
+}
+playerTwoRock?.addEventListener('click', clickOnRockPlayerTwo)
+
+function clickOnPaperPlayerTwo() {
+  playerTwoChoice = 'paper'
+  console.log(playerTwoChoice)
+}
+playerTwoPaper?.addEventListener('click', clickOnPaperPlayerTwo)
+
+function clickOnScissorsPlayerTwo() {
+  playerTwoChoice = 'scissors'
+  console.log(playerTwoChoice)
+}
+playerTwoScissors?.addEventListener('click', clickOnScissorsPlayerTwo)
+
 // Display winner
