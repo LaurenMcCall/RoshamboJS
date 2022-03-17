2b15af3c398a400ca99f808199a4544cd1a351ec
Lauren McCall <lmmccolloch@gmail.com>
2022-03-17 12:17:24 -0400

    winner displays now

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,8 @@ const playerTwoRock = document.querySelector('.player2 i.rock')
 const playerTwoPaper = document.querySelector('.player2 i.paper')
 const playerTwoScissors = document.querySelector('.player2 i.scissors')
 
+const displayWinner = document.querySelector('h1')
+
 let playerOneChoice: string
 let playerTwoChoice: string
 let result: string
@@ -20,37 +22,76 @@ let result: string
 function clickOnRockPlayerOne() {
   playerOneChoice = 'rock'
   console.log(playerOneChoice)
+  // QUESTION: Only way to not have player one return a winner until after player two
+  // selects is to remove the below. is there a way to make it so whichever player
+  // goes last/second the winner is returned?
+  // returnWinner()
+  // console.log(returnWinner)
 }
 playerOneRock?.addEventListener('click', clickOnRockPlayerOne)
 
 function clickOnPaperPlayerOne() {
   playerOneChoice = 'paper'
   console.log(playerOneChoice)
+  // returnWinner()
+  // console.log(returnWinner)
 }
 playerOnePaper?.addEventListener('click', clickOnPaperPlayerOne)
 
 function clickOnScissorsPlayerOne() {
   playerOneChoice = 'scissors'
   console.log(playerOneChoice)
+  // returnWinner()
+  // console.log(returnWinner)
 }
 playerOneScissors?.addEventListener('click', clickOnScissorsPlayerOne)
 
 function clickOnRockPlayerTwo() {
   playerTwoChoice = 'rock'
   console.log(playerTwoChoice)
+  returnWinner()
+  console.log(returnWinner)
 }
 playerTwoRock?.addEventListener('click', clickOnRockPlayerTwo)
 
 function clickOnPaperPlayerTwo() {
   playerTwoChoice = 'paper'
   console.log(playerTwoChoice)
+  returnWinner()
+  console.log(returnWinner)
 }
 playerTwoPaper?.addEventListener('click', clickOnPaperPlayerTwo)
 
 function clickOnScissorsPlayerTwo() {
   playerTwoChoice = 'scissors'
   console.log(playerTwoChoice)
+  returnWinner()
+  console.log(returnWinner)
 }
 playerTwoScissors?.addEventListener('click', clickOnScissorsPlayerTwo)
 
 // Display winner
+function returnWinner() {
+  if (playerOneChoice === playerTwoChoice) {
+    result = "✨It's a draw! Prepare your wands to duel again!✨"
+  }
+  if (playerOneChoice === 'rock' && playerTwoChoice === 'scissors') {
+    result = '✨Witch 1 wins!✨'
+  }
+  if (playerOneChoice === 'rock' && playerTwoChoice === 'paper') {
+    result = '✨Witch 2 wins!✨'
+  }
+  if (playerOneChoice === 'paper' && playerTwoChoice === 'rock') {
+    result = '✨Witch 1 wins!✨'
+  }
+  if (playerOneChoice === 'paper' && playerTwoChoice === 'scissors') {
+    result = '✨Witch 2 wins!✨'
+  }
+  if (playerOneChoice === 'scissors' && playerTwoChoice === 'rock') {
+    result = '✨Witch 2 wins!✨'
+  }
+  if (playerOneChoice === 'scissors' && playerTwoChoice === 'paper') {
+    result = '✨Witch 1 wins!✨'
+  }
+  displayWinner!.innerHTML = result
+}
